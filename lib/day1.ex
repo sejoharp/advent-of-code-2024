a7cde97d11196eaf3f1eafe7e72646e0333c7fe9
Joscha <zippelman@web.de>
2024-11-30 23:14:18 +0100

    project setup

---
+++ lib/day1.ex
@@ -0,0 +1,7 @@
+defmodule Day1 do
+  @moduledoc false
+
+  def bla do
+    :blub
+  end
+end
---
+++ test/day1_test.exs
@@ -0,0 +1,7 @@
+defmodule Day1Test do
+  use ExUnit.Case
+
+  test "greets the world" do
+    assert Day1.bla() == :blub
+  end
+end
