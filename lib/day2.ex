c7c44b4f6a2753e027bbbf3d707ebe58e1a40c91
Joscha <zippelman@web.de>
2024-12-07 00:48:59 +0100

    solve day two part 1

---
+++ lib/day2.ex
@@ -0,0 +1,79 @@
+defmodule Day2 do
+  @moduledoc false
+
+  def convert_string_to_number(string_value) do
+    String.to_integer(string_value)
+  end
+
+  def read_input do
+    input_path = "lib/day2_input.txt"
+    case File.open(input_path, [:read]) do
+      {:ok, file} ->
+        lines =
+          IO.stream(file, :line)
+          |> Enum.to_list()
+          |> Enum.map(fn line ->
+            line
+            |> (fn line -> String.replace(line, "\n", "") end).()
+            |> (fn line -> String.split(line, " ") end).()
+            |> Enum.map(fn string_numbers -> convert_string_to_number(string_numbers) end)
+          end)
+
+        File.close(file)
+        lines
+
+      {:error, reason} ->
+        IO.puts("Failed to open file: #{reason}")
+    end
+  end
+
+  def detect_sorting([first, second | _numbers]) when first < second do
+    :increase
+  end
+
+  def detect_sorting([first, second | _numbers]) when first > second do
+    :decrease
+  end
+
+  def detect_sorting([first, second | _numbers]) when first == second do
+    :equal
+  end
+
+  def detect_state([first | numbers], sorting) when sorting == :increase do
+    second = List.first(numbers)
+
+    cond do
+      second >= first + 1 and second <= first + 3 and length(numbers) == 1 -> :safe
+      second >= first + 1 and second <= first + 3 -> detect_state(numbers, :increase)
+      true -> :unsafe
+    end
+  end
+
+  def detect_state([first | numbers], sorting) when sorting == :decrease do
+    second = List.first(numbers)
+
+    cond do
+      second <= first - 1 and second >= first - 3 and length(numbers) == 1 -> :safe
+      second <= first - 1 and second >= first - 3 -> detect_state(numbers, :decrease)
+      true -> :unsafe
+    end
+  end
+
+  def detect_state(_numbers, sorting) when sorting == :equal do
+    :unsafe
+  end
+
+  def is_safe?(numbers) do
+    sorting = detect_sorting(numbers)
+    detect_state(numbers, sorting)
+  end
+
+  def count_safe_lists(number_lists) do
+    safe_lists =
+      Enum.filter(number_lists, fn number_list ->
+        is_safe?(number_list) == :safe
+      end)
+
+    length(safe_lists)
+  end
+end
